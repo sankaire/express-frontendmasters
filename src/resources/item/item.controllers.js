c821f401d60e04672e874e73d6f691f24e52cb05
Carver <kerenketepela@gmail.com>
2022-03-01 13:05:49 +0300

    create a new item

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,18 @@
-export default {}
+import { Item } from './item.model'
+import mongoose from 'mongoose'
+import { connect } from '../../utils/db'
+
+const create = async () => {
+  await connect(
+    'mongodb+srv://tepela:ZAS0706x@cluster0.ns3va.mongodb.net/frontendmasters?retryWrites=true&w=majority'
+  )
+  const newItem = await Item.create({
+    name: 'test',
+    createdBy: mongoose.Types.ObjectId(),
+    list: mongoose.SchemaTypes.ObjectId()
+  })
+  console.log(newItem)
+}
+
+create()
+// export default {}
