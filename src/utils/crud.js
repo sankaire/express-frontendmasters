267ed0344cca90dc6bfa4df9f894d570f65d62a4
Carver <kerenketepela@gmail.com>
2022-03-07 14:35:37 +0300

    feat:create a generic crud controller

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,12 +1,52 @@
-export const getOne = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.params.user._id
 
-export const getMany = model => async (req, res) => {}
+  const doc = await model.findOne({ _id: id, createdBy: userId }).exec()
 
-export const createOne = model => async (req, res) => {}
+  if (!doc) {
+    res.status(404).end()
+  }
+  res.status(200).json({ data: doc })
+}
 
-export const updateOne = model => async (req, res) => {}
+export const getMany = model => async (req, res) => {
+  const docs = await model.find({ createdBy: req.user._id })
+  res.status(200).json({ data: docs })
+}
 
-export const removeOne = model => async (req, res) => {}
+export const createOne = model => async (req, res) => {
+  const doc = await model.create({ ...req.body, createdBy: req.user._id })
+  res.status(200).json({ data: doc })
+}
+
+export const updateOne = model => async (req, res) => {
+  const doc = await model.findOneAndUpdate(
+    {
+      _id: req.params.id,
+      createdBy: req.user._id
+    },
+    req.body,
+    { new: true }
+  )
+  if (!doc) {
+    return res.status(400).end()
+  }
+  res.status(200).json({ data: doc })
+}
+
+export const removeOne = model => async (req, res) => {
+  const doc = await model
+    .findOneAndRemove({
+      _id: req.params._id,
+      createdBy: req.user._id
+    })
+    .exec()
+  if (!doc) {
+    return res.status(400).end()
+  }
+  res.status(200).json({ data: doc })
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
