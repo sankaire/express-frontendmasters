f85e0761a1efb85e66cfdfd28d073fc9169b4f06
Carver <kerenketepela@gmail.com>
2022-03-07 14:36:28 +0300

    feat:pull in the lists router

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
 import cors from 'cors'
-
+import { listRouter } from '../src/resources/list/list.router'
 export const app = express()
 
 app.disable('x-powered-by')
@@ -11,13 +11,8 @@ app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
+app.use('api/v1/lists', listRouter)
 
-app.get('/data', (req, res) => {
-  res.send({ message: 'Hello' })
-})
-app.post('/data', (req, res) => {
-  res.send(req.body)
-})
 export const start = () => {
-  app.listen(5001, console.log(`server running on 5000`))
+  app.listen(5000, console.log(`server running on 5000`))
 }
