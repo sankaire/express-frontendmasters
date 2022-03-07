7e334829ec3c941f130d19ab8f928f0330bb61e5
Carver <kerenketepela@gmail.com>
2022-03-07 14:34:35 +0300

    feat:use crud controller

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,18 +1,4 @@
 import { Item } from './item.model'
-import mongoose from 'mongoose'
-import { connect } from '../../utils/db'
+import { crudControllers } from '../../utils/crud'
 
-const create = async () => {
-  await connect(
-    'mongodb+srv://tepela:ZAS0706x@cluster0.ns3va.mongodb.net/frontendmasters?retryWrites=true&w=majority'
-  )
-  const newItem = await Item.create({
-    name: 'test',
-    createdBy: mongoose.Types.ObjectId(),
-    list: mongoose.SchemaTypes.ObjectId()
-  })
-  console.log(newItem)
-}
-
-create()
-// export default {}
+export default crudControllers(Item)
